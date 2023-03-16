4d98f0b8faf2cf157bb6011aba8b651126755721
perocha <pedrojrocha@outlook.com>
2023-03-16 19:29:52 +0100

    Updated resource group name

--- key_vault.tf
+++ key_vault.tf
@@ -14,7 +14,7 @@ module "key_vault" {
   source = "github.com/Azure/azure-data-labs-modules/terraform/key-vault"
 
   basename                 = local.basename
-  resource_group           = module.resource_group.name
+  resource_group_name      = module.resource_group.name
   location                 = local.location
   sku_name                 = var.sku_name
   purge_protection_enabled = var.purge_protection_enabled
