cd59a6dae77548643ca57f549fbed21d300a2a8e
perocha <pedrojrocha@outlook.com>
2023-03-16 19:13:17 +0100

    Update input var in key vault module

--- modules/key_vault/key_vault.tf
+++ modules/key_vault/key_vault.tf
@@ -4,7 +4,7 @@ module "key_vault" {
   source = "github.com/Azure/azure-data-labs-modules/terraform/key-vault"
 
   basename                 = local.basename
-#  rg_name                  = var.resource_group_name
+  rg_name                  = module.resource_group.name
   location                 = local.location
   sku_name                 = var.sku_name
   purge_protection_enabled = var.purge_protection_enabled
