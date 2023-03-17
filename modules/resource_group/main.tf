e83e42e680adcac6611652be1295297cba161052
perocha <pedrojrocha@outlook.com>
2023-03-17 08:47:19 +0100

    Updating folder structure

--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -3,7 +3,4 @@
 module "resource_group" {
   source = "github.com/Azure/azure-data-labs-modules/terraform/resource-group"
 
-  basename = local.basename
-  location = local.location
-  tags = local.tags
 }
