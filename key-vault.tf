8fe11705fc4413f847b3203850e261c3050dbb39
Walter Grasselli <waltergrasselli@gmail.com>
2023-03-17 14:23:06 +0100

    .

--- key-vault.tf
+++ key-vault.tf
@@ -1,8 +1,10 @@
 module "key_vault" {
-  source              = "../modules/key-vault"
+  source              = "github.com/Azure/azure-data-labs-modules/terraform/key-vault" #"../modules/key-vault"
   basename            = random_string.postfix.result
-  resource_group_name = module.local_rg.name
+  resource_group_name = module.resource_group.name
   location            = var.location
+  module_enabled      = 1
+  is_sec_module       = 0
   #subnet_id            = module.local_snet_default.id
   #private_dns_zone_ids = [module.local_pdnsz_kv.list[local.dns_kv_vault].id]
   sku_name = var.sku_name
