f98ac9a580f2b8d440c7f423acffb4d5ec2ee425
Walter Grasselli <38637309+waltergrasselli@users.noreply.github.com>
2023-03-18 17:38:34 +0100

    fix conflicts

--- modules/resource-group/variables.tf
+++ modules/resource-group/variables.tf
@@ -5,7 +5,6 @@ variable "basename" {
     error_message = "Resource group names must be between 0 and 87 characters and can only include alphanumeric, underscore, parentheses, hyphen, period (except at end)."
   }
   description = "Basename of the module."
-  default     = "fhir-001"
 }
 
 variable "location" {
@@ -24,16 +23,4 @@ variable "module_enabled" {
   type        = bool
   description = "Variable to enable or disable the module."
   default     = true
-}
-
-variable "prefix" {
-  type        = string
-  description = "Prefix for module names"
-  default     = "fhir-prefix"
-}
-
-variable "postfix" {
-  type        = string
-  description = "Postfix for module names"
-  default     = "101"
 }
