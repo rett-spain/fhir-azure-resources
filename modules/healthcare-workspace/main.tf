2f058ebdafb286a831103f420dd5e3f5e28e7c91
perocha <pedrojrocha@outlook.com>
2023-03-19 22:37:05 +0100

    Remove hyphen from healthcare workspace name

--- modules/healthcare-workspace/main.tf
+++ modules/healthcare-workspace/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_healthcare_workspace" "azure_health" {
-  name                          = "ahcw-${var.basename}"
+  name                          = "ahcw${var.basename}"
   location                      = var.location
   resource_group_name           = var.resource_group_name
   tags                          = var.tags
