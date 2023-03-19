14656a477ef5a9ded3e64cfebe1add53850f038d
perocha <pedrojrocha@outlook.com>
2023-03-19 22:11:40 +0100

    Update healthcare workspace module

--- modules/healthcare-workspace/main.tf
+++ modules/healthcare-workspace/main.tf
@@ -1,8 +1,8 @@
 resource "azurerm_healthcare_workspace" "azure_health" {
-  name                          = "redis-${var.basename}"
+  name                          = "ahcw-${var.basename}"
   location                      = var.location
   resource_group_name           = var.resource_group_name
-  tags = var.tags
+  tags                          = var.tags
 
   count = var.module_enabled ? 1 : 0
 }
--- modules/healthcare-workspace/variables.tf
+++ modules/healthcare-workspace/variables.tf
@@ -19,6 +19,15 @@ variable "tags" {
   description = "A mapping of tags which should be assigned to the Resource Group."
 }
 
+variable "resource_group_name" {
+  type        = string
+  description = "Resource group name."
+  validation {
+    condition     = can(regex("^[-\\w\\.\\(\\)]{1,90}$", var.resource_group_name)) && can(regex("[-\\w\\(\\)]+$", var.resource_group_name))
+    error_message = "Resource group names must be between 1 and 90 characters and can only include alphanumeric, underscore, parentheses, hyphen, period (except at end)."
+  }
+}
+
 variable "module_enabled" {
   type        = bool
   description = "Variable to enable or disable the module."
